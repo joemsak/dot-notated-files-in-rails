0348b51c0a32a2eed179a4738a2cb3a62d9ee0bd
Joe Sak <joe@joesak.com>
2021-01-22 14:22:21 -0600

    Initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//
+// If I rename this file to knockout.js and require knockout, it will work fine
+// That is not a satisfying solution as my real project is a legacy app we are
+// trying to upgrade, and has many dot-notated filenames.
+//
+//= require knockout.debug
